f7ad3c0da02de62ef939e79e902cdd531bb492c5
kostasp31 <kostaspetrakis2003@gmail.com>
2024-12-21 22:45:40 +0200

    main page navbar

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -5,8 +5,22 @@ const Root = () => {
 
   return (
     <div>
-      <button  onClick={() => navigate('/register')}>Εγγραφή</button>
-      <button  onClick={() => navigate('/login')}>Σύνδεση</button>
+      <nav class="navbar">
+        <a href="/" class="logo">
+          <img src="/logos/gov_gr_logo.svg" alt="GOV logo" />
+        </a>
+        <ul class="nav-links">
+          <li class="nav-item"><a href="#">Αρχική σελίδα</a></li>
+          <li class="nav-item"><a href="#">Επαγγελματίες</a></li>
+          <li class="nav-item"><a href="#">Γονείς</a></li>
+          <li class="nav-item"><a href="#">Ανακοινώσεις</a></li>
+          <li class="nav-item"><a href="#">Βοήθεια</a></li>
+          <li class="nav-item"><a href="/login">Σύνδεση</a></li>
+        </ul>
+      </nav>
+
+      {/* <button  onClick={() => navigate('/register')}>Εγγραφή</button>
+      <button  onClick={() => navigate('/login')}>Σύνδεση</button> */}
     </div>
   )
 }

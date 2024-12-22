3951c7804f451458a005c0de166724d0f3617cf5
kostasp31 <kostaspetrakis2003@gmail.com>
2024-12-23 00:15:38 +0200

    building main page

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -16,11 +16,48 @@ const Root = () => {
           <li class="nav-item"><a href="#">Ανακοινώσεις</a></li>
           <li class="nav-item"><a href="#">Βοήθεια</a></li>
           <li class="nav-item"><a href="/login">Σύνδεση</a></li>
+          {/* <li class="nav-item-log"><a href="/register">Εγγραφή</a></li> */}
         </ul>
       </nav>
 
-      {/* <button  onClick={() => navigate('/register')}>Εγγραφή</button>
-      <button  onClick={() => navigate('/login')}>Σύνδεση</button> */}
+      {/* height = 100vh for above the fold*/}
+      <div style={{height:'90vh', backgroundColor:'white'}}>
+
+        <div className='main-elements'>
+          <div className='personas-buttons'>
+            <div style={{display:'flex', flexDirection:'row', gap:'5px'}}>
+              <button className='persona1-button'>Για γονείς</button>
+              <button className='persona2-button'>Για επαγγελματίες</button>
+            </div>
+          </div>
+          <div className='searchbar-main'>
+            <div className='searchbar-main-icon'>
+              <img src='/icons/calendar_32.png' />
+            </div>
+            <div style={{display:'flex', alignItems:'center'}}>
+              <input
+                className='searchbar-main-input'
+                placeholder='Ημέρες / Ώρες'
+              />
+            </div>
+            <div className='searchbar-main-icon'>
+              <img src='/icons/vertical-line_32.png' />
+            </div>
+            <div className='searchbar-main-icon'>
+              <img src='/icons/location_32.png' />
+            </div>
+            <div style={{display:'flex', alignItems:'center'}}>
+              <input
+                className='searchbar-main-input'
+                placeholder='Τοποθεσία'
+              />
+            </div>
+            <div className='searchbar-main-icon'>
+              <img src='/icons/search_32.png' />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
